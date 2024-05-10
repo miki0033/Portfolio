4624be1941aa4257ac267239467b05215d1b3626
miki0033 <75072281+miki0033@users.noreply.github.com>
2024-05-10 11:40:45 +0200

    Deploy React app to GitHub Pages

--- src/components/Icon.tsx
+++
@@ -1,9 +0,0 @@
-const Icon = (props: { image: string }) => {
-  return (
-    <div>
-      <img src={props.image} />
-    </div>
-  );
-};
-
-export default Icon;
--- src/components/Scroll_linked.tsx
+++
@@ -1,9 +0,0 @@
-import { motion, useScroll } from "framer-motion";
-
-function Scroll_linked() {
-  const { scrollYProgress } = useScroll();
-
-  return <motion.h2 style={{ scaleX: scrollYProgress }} />;
-}
-
-export default Scroll_linked;
